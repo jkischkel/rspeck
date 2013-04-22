fb26007585a034e81346e65c6c14d2e70b613465
jkischkel <jk@soundcloud.com>
2013-04-22 19:51:52 +0200

    fix regex problems

--- lib/finder.rb
+++ lib/finder.rb
@@ -8,14 +8,15 @@ module Spectr
     end
 
     def find(pattern)
-      results = `#{detect_finder} \"#{pattern}\" spec/ --ruby`
+      regex = /(\W|\A)it\W.*#{pattern}/
+      results = `#{detect_finder} \"#{regex}\" spec/ --ruby`
   
       results.split(/\n/).map do |line|
         parts = line.split(/:/).map(&:chomp).map(&:strip)
 
         { :file => parts[0],
           :line => parts[1],
-          :spec => /"(.+)"/.match(parts[2]).captures.first }
+          :spec => parts[2] }
       end
     end
   end
